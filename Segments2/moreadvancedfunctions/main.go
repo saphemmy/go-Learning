2213b7d6d7c383f401c3f14e82149075bb6153fa
Oluwafemi Sosami <saphemmy@gmail.com>
2023-06-30 14:19:59 +0100

    advanced functions

---
+++ Segments2/moreadvancedfunctions/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Main function runs child function
+// It's often used to run authentication
+func main()  {
+	testing()
+}
+
+// Here is the child function
+func testing()  {
+	fmt.Println("hello from the testing function")
+}
